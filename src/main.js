a267641a019f107c397f6118a9bc1ebbdbe55a99
Amabel <luoweibinb@gmail.com>
2019-09-17 17:28:55 +0900

    feat: add basic structure

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import store from './store/store'
+
+import './common/styles/global.scss'
 
 Vue.config.productionTip = false
 
---
+++ src/pages/Home/Home.vue
@@ -0,0 +1,3 @@
+<template src="./Home.vue.html"></template>
+<script src="./Home.vue.js"></script>
+<style src="./Home.vue.scss" lang="scss"></style>
